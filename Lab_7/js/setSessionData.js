d7e58115ff431be85838076e515e8b4509cfcef6
Ammara Ramzan <ammara.ramzan1@gmail.com>
2020-11-06 22:51:28 -0500

    commit

---
+++ Lab_7/js/setSessionData.js
@@ -0,0 +1,23 @@
+const main = document.getElementById("main");
+
+function getBusRoute() {
+  let busRoute = "104"; // Your code here
+
+  if ((typeof busRoute !== "undefined") & (busRoute !== "")) {
+    let busRouteURL = "https://api.umd.io/v0/bus/routes"; // Your code here
+
+    fetch(busRouteURL)
+      .then((response) => {
+        return response.json();
+      })
+      .then((route) => {
+        // YOUR CODE HERE
+      })
+      .catch((err) => {
+        console.log(err);
+        main.innerHTML = "Invalid bus route";
+      });
+  } else {
+    main.innerHTML = "No value provided";
+  }
+}
