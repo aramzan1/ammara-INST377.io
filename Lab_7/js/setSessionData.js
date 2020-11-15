4a42e7e04945896454604cc4e41e9e19a096c606
Ammara Ramzan <ammara.ramzan1@gmail.com>
2020-11-15 16:13:48 -0500

    Update setSessionData.js

--- Lab_7/js/setSessionData.js
+++ Lab_7/js/setSessionData.js
@@ -1,18 +1,44 @@
 const main = document.getElementById("main");
 
 function getBusRoute() {
-  let busRoute = "104"; // Your code here
+  let busRoute = document.getElementById("busroute").value;; // Your code here
 
   if ((typeof busRoute !== "undefined") & (busRoute !== "")) {
-    let busRouteURL = "https://api.umd.io/v0/bus/routes"; // Your code here
-
+    let busRouteURL = "https://api.umd.io/v0/bus/routes"+ busRoute;
+    console.log(busURL); // Your code here
+   // const fetchPromise = fetch(busURL);
     fetch(busRouteURL)
       .then((response) => {
         return response.json();
       })
       .then((route) => {
-        // YOUR CODE HERE
-      })
+        console.log("Here");
+        console.log(route);
+        let title, latMax, latMin, lonMax, lonMin;
+        let message = "";
+        if (typeof route.title !== "undefined") {
+          title = route.title;
+          latMax = route.lat_max;
+          latMin = route.lat_min;
+          lonMax = route.lon_max;
+          lonMin = route.lon_min;
+          message =
+            title +
+            " <br>Lat max : " +
+            latMax +
+            " <br>Lat min : " +
+            latMin +
+            " <br>Lon Max : " +
+            lonMax +
+            " <br>Lon Min :" +
+            lonMin;
+        } else {
+          message = "No bus info available";
+        }
+        console.log(title);
+        // Append names to main element
+        main.innerHTML = "Bus Route Info: " + message;
+      })// YOUR CODE HERE
       .catch((err) => {
         console.log(err);
         main.innerHTML = "Invalid bus route";
