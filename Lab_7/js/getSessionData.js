b39d0d90017a2dd77b405e2ed2477c839386f908
Ammara Ramzan <ammara.ramzan1@gmail.com>
2020-11-20 23:51:55 -0500

    commit

--- Lab_7/js/getSessionData.js
+++ Lab_7/js/getSessionData.js
@@ -1,4 +1,21 @@
 // Get saved data from sessionStorage
+
+<b>Title: </b>
+<p id="title">Nothing set</p>
+<br /><br />
+<b>Maximum latitude: </b>
+<p id="lat_max">Nothing set</p>
+<br /><br />
+<b>Minimum latitude: </b>
+<p id="lat_min">Nothing set</p>
+<br /><br />
+<b>Maximum longitude:</b>
+<p id="lon_max">Nothing set</p>
+<br /><br />
+<b>Minimum longitude:</b>
+<p id="lon_min">Nothing set</p>
+<br /><br />
+<a href="index.html">Back to index.html</a>
 let title = ""; // Your code here
 let lat_max = "";
 let lat_min = "";
